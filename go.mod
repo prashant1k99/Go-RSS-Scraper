56d234a3de3e47dad08791b8127111df6aad456a
Prashant Singh <prashantco111@gmail.com>
2024-08-22 15:03:06 +0530

    chore: Add server setup and CORS configuration

--- go.mod
+++ go.mod
@@ -3,3 +3,7 @@ module github.com/prashant1k99/Go-RSS-Scraper
 go 1.22.6
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5
+
+require github.com/go-chi/cors v1.2.1
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -17,5 +20,32 @@ func main() {
 	if PORT == "" {
 		log.Fatal("$PORT must be set")
 	}
-	fmt.Println("Server is running on port: ", PORT)
+
+	router := chi.NewRouter()
+
+	// Cores setup
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + PORT,
+	}
+
+	fmt.Printf("Server is running on port: http://localhost:%v", PORT)
+	err := srv.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+
 }
